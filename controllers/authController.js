3a69bdc0a73ff8b930fc3d107daf98f1972fa184
salemhella <salem_hellal@protonmail.com>
2024-05-20 15:36:39 +0100

    add auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,96 +7,93 @@ const Account = require('../models/accountModel');
 
 // Signup
 async function signUp(req, res) {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-
-  const { user, account } = req.body;
-
-  const { nom, prenom, email, telephone, pays, ville, adresse, contact, type } = user;
-  const { accountIdentifier, accountType, password } = account;
-
-  try {
-    let existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res.status(400).json({ message: 'This user already exists.' });
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-
-    let existingAccount = await Account.findOne({ accountIdentifier });
-    if (existingAccount) {
-      return res.status(400).json({ message: 'An account with this identifier already exists.' });
+  
+    const { user, account } = req.body;
+  
+    const { nom, prenom, email, telephone, pays, ville, adresse, contact, type } = user;
+    const { accountType, password } = account;
+  
+    try {
+      let existingUser = await User.findOne({ email });
+      if (existingUser) {
+        return res.status(400).json({ message: 'This user already exists.' });
+      }
+  
+      const hashedPassword = await bcrypt.hash(password, 10);
+  
+      const newUser = new User({
+        nom,
+        prenom,
+        email,
+        telephone,
+        pays,
+        ville,
+        adresse,
+        contact,
+        type,
+      });
+  
+      const newAccount = new Account({
+        user: newUser._id,
+        accountIdentifier: email, // Using email as accountIdentifier
+        accountType,
+        password: hashedPassword,
+        registrationDate: Date.now(),
+        status: 'active',
+        accountSettings: {
+          notifications: true,
+          privacy: 'public',
+        },
+      });
+  
+      await newAccount.save();
+      await newUser.save();
+  
+      res.status(201).json({ success: true, message: 'Registration successful.' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ success: false, message: 'Error during registration.' });
     }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const newUser = new User({
-      nom,
-      prenom,
-      email,
-      telephone,
-      pays,
-      ville,
-      adresse,
-      contact,
-      type,
-    });
-
-    const newAccount = new Account({
-      user: newUser._id,
-      accountIdentifier,
-      accountType,
-      password: hashedPassword,
-      registrationDate: Date.now(),
-      status: 'active',
-      accountSettings: {
-        notifications: true,
-        privacy: 'public',
-      },
-    });
-
-    await newAccount.save();
-    await newUser.save();
-
-    res.status(201).json({ success: true, message: 'Registration successful.' });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ success: false, message: 'Error during registration.' });
   }
-}
-
+  
 // Login
 const login = async (req, res) => {
-  try {
-    const { accountIdentifier, password } = req.body;
-
-    const account = await Account.findOne({ accountIdentifier });
-    if (!account) {
-      return res.status(401).json({ error: 'No account found with this identifier' });
-    }
-
-    
-    bcrypt.hash('mypassword', 10, function(err, hash) {
-        if (err) { throw (err); }
-    
-        bcrypt.compare('mypassword', hash, function(err, result) {
-            if (err) { return res.status(401).json({ error: 'Incorrect password' }); }
-            console.log(result);
+    try {
+      const { accountIdentifier,email , password } = req.body;
+  
+      const account = await Account.findOne({
+          $or: [{ accountIdentifier }, { email }]
         });
-    });
-   
-    const JWT_SECRET = process.env.JWT_SECRET;
-    const token = jwt.sign({ accountId: account._id }, JWT_SECRET, { expiresIn: '1h' });
-    console.log("testtest")
-    res.cookie("app-session-token", token, { expire: Date.now() + 86400 });
-
-    res.status(200).json({ token });
-  } catch (error) {
-    console.error('Authentication Error:', error);
-    res.status(500).json({ error: 'Authentication error' });
-  }
-};
-
+    
+      if (!account) {
+         return res.status(401).json({ error: 'No account found with this identifier or email' });
+      }
+      
+      bcrypt.hash('mypassword', 10, function(err, hash) {
+          if (err) { throw (err); }
+      
+          bcrypt.compare('mypassword', hash, function(err, result) {
+              if (err) { return res.status(401).json({ error: 'Incorrect password' }); }
+              console.log(result);
+          });
+      });
+     
+      const JWT_SECRET = process.env.JWT_SECRET;
+      const token = jwt.sign({ accountId: account._id }, JWT_SECRET, { expiresIn: '1h' });
+      console.log("testtest")
+      res.cookie("app-session-token", token, { expire: Date.now() + 86400 });
+  
+      res.status(200).json({ token });
+    } catch (error) {
+      console.error('Authentication Error:', error);
+      res.status(500).json({ error: 'Authentication error' });
+    }
+  };
+  
 // Logout
 const logout = async (req, res) => {
   try {
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express =require("express");   
 const dotenv = require("dotenv").config();
 const app = express();
+const cors = require("cors");
 const  userroutes =require('./routes/userRoutes');
 const clientRouter = require('./routes/clientRouter');
 const produitRoutes = require('./routes/produitRoute');
@@ -26,14 +27,16 @@ mongoose.connect(uri)
     console.log("DB connection failed with - ", err);
   });
   app.use(express.json());
-
+app.use(cors({
+  origin: 'http://localhost:5173'
+}));
  app.use('/api/auth', auth);
  app.use('/api/users',userroutes);
  app.use('/api/clients', clientRouter);
  app.use('/api/produits', produitRoutes);
 
  // SERVER LISTENING
- const port = process.env.PORT || 6666;
+ const port = process.env.PORT || 8080;
 
  app.listen(port ,() => {
     console.log(`server running on port ${port}`);
