88a87ec797a13122815c513653a89f09c858c3a5
Samar Slim <samarslim64@gmail.com>
2024-06-18 11:59:57 +0100

    db

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.use(cors({
 mongoose.Promise = global.Promise;
 
 
-mongoose.connect("mongodb+srv://samarslim64:s24042002@cluster0.ogzjsac.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+mongoose.connect(process.env.db_name)
   .then(() => {
     console.log("DB connected");
   })
