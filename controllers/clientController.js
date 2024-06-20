266b54d073cb58eccc219a07cbe74f8f62d661aa
salemhella <salem_hellal@protonmail.com>
2024-06-20 23:33:47 +0100

    get client devis

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -185,6 +185,19 @@ const deleteClient = async (req, res) => {
 
 };
 
+const getClientByEmail = async (req, res) => {
+  const email = req.params.email;
+  try {
+    const client = await Client.findOne({ email });
+    if (!client) {  
+      return res.status(404).json({ message: 'Client not found' });
+    } 
+    res.json(client);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  } 
+};
+
 const updateClient = async (req, res) => {
   try {
     const updatedClient = await Client.findByIdAndUpdate(req.params.id, req.body, { new: true });
@@ -197,4 +210,4 @@ const updateClient = async (req, res) => {
   }
 };
 
-module.exports = { generateStrongPassword, transporter, getAllClient, getClientById, createClient, deleteClient, updateClient };
+module.exports = { generateStrongPassword, transporter, getAllClient, getClientById, createClient, deleteClient, updateClient , getClientByEmail };
